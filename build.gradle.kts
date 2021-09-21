1305b0d027e9151fcabfb22a8900f277ed4731ce
Haru <64423645+harulol@users.noreply.github.com>
2021-09-21 12:29:51 +0900

    Updated versioning and dependabot.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ allprojects {
 
     dependencies {
         implementation("org.jetbrains:annotations:22.0.0")
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
     }
 
     tasks.test {
@@ -53,7 +53,7 @@ project(":nbt") {
 }
 
 project(":inventories") {
-    version = "1.0-SNAPSHOT"
+    version = "2.0-SNAPSHOT"
 }
 
 
