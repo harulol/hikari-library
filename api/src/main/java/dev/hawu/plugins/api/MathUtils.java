91a325139cf669dab036e76c76f5b2b5ab26a621
Haru <64423645+harulol@users.noreply.github.com>
2022-03-30 17:28:19 +0900

    More vector stuff I stole online

--- api/src/main/java/dev/hawu/plugins/api/MathUtils.java
+++ api/src/main/java/dev/hawu/plugins/api/MathUtils.java
@@ -36,4 +36,130 @@ public final class MathUtils {
         else return vector.normalize();
     }
 
+    /**
+     * Rotates the provided vector around the X axis.
+     *
+     * @param v     the vector to rotate
+     * @param angle the angle to rotate by (in degrees)
+     * @return the rotated vector
+     * @since 1.6
+     */
+    @NotNull
+    public static Vector rotateAroundAxisX(final @NotNull Vector v, final double angle) {
+        final double radians = Math.toRadians(angle);
+        return rotateAroundAxisX(v, Math.cos(radians), Math.sin(radians));
+    }
+
+    /**
+     * Rotates the provided vector around the Y axis.
+     *
+     * @param v     the vector to rotate
+     * @param angle the angle to rotate by (in degrees)
+     * @return the rotated vector
+     * @since 1.6
+     */
+    @NotNull
+    public static Vector rotateAroundAxisY(final @NotNull Vector v, final double angle) {
+        final double radians = Math.toRadians(-angle);
+        return rotateAroundAxisY(v, Math.cos(radians), Math.sin(radians));
+    }
+
+    /**
+     * Rotates the provided vector around the Z axis.
+     *
+     * @param v     the vector to rotate
+     * @param angle the angle to rotate by (in degrees)
+     * @return the rotated vector
+     * @since 1.6
+     */
+    @NotNull
+    public static Vector rotateAroundAxisZ(final @NotNull Vector v, final double angle) {
+        final double radians = Math.toRadians(angle);
+        return rotateAroundAxisZ(v, Math.cos(radians), Math.sin(radians));
+    }
+
+    /**
+     * Rotates the provided vector around the X axis,
+     * with trig functions pre-calculated. Usually for performance reasons.
+     *
+     * @param v   the vector to rotate
+     * @param cos the cosine of the angle to rotate by
+     * @param sin the sine of the angle to rotate by
+     * @return the rotated vector
+     * @author finnbon
+     * @since 1.6
+     */
+    @NotNull
+    public static Vector rotateAroundAxisX(final @NotNull Vector v, final double cos, final double sin) {
+        double y = v.getY() * cos - v.getZ() * sin;
+        double z = v.getY() * sin + v.getZ() * cos;
+        return v.setY(y).setZ(z);
+    }
+
+    /**
+     * Rotates the provided vector around the Y axis,
+     * with trig functions pre-calculated. Usually for performance reasons.
+     *
+     * @param v   the vector to rotate
+     * @param cos the cosine of the angle to rotate by
+     * @param sin the sine of the angle to rotate by
+     * @return the rotated vector
+     * @author finnbon
+     * @since 1.6
+     */
+    @NotNull
+    public static Vector rotateAroundAxisY(final @NotNull Vector v, final double cos, final double sin) {
+        double x = v.getX() * cos + v.getZ() * sin;
+        double z = v.getX() * -sin + v.getZ() * cos;
+        return v.setX(x).setZ(z);
+    }
+
+    /**
+     * Rotates the provided vector around the Z axis,
+     * with trig functions pre-calculated. Usually for performance reasons.
+     *
+     * @param v   the vector to rotate
+     * @param cos the cosine of the angle to rotate by
+     * @param sin the sine of the angle to rotate by
+     * @return the rotated vector
+     * @author finnbon
+     * @since 1.6
+     */
+    @NotNull
+    public static Vector rotateAroundAxisZ(final @NotNull Vector v, final double cos, final double sin) {
+        double x = v.getX() * cos - v.getY() * sin;
+        double y = v.getX() * sin + v.getY() * cos;
+        return v.setX(x).setY(y);
+    }
+
+    /**
+     * Gets the perpendicular part of vector u on vector onto.
+     * This does not mutate the original vectors.
+     *
+     * @param u    the vector to find the perpendicular part of
+     * @param onto the vector to find the perpendicular part on
+     * @return the perpendicular part of vector u on vector onto.
+     * @author SHUTUPANDUSECAPS
+     * @since 1.6
+     */
+    @NotNull
+    public static Vector perpendicular(final @NotNull Vector u, final @NotNull Vector onto) {
+        return u.clone().subtract(projection(onto, u));
+    }
+
+    /**
+     * Gets the projection of vector u on vector onto.
+     * This does not mutate the original vectors.
+     *
+     * @param u    the vector to find the projection of
+     * @param onto the vector to find the projection on
+     * @return the projection of vector u on vector onto.
+     * @author SHUTUPANDUSECAPS
+     * @since 1.6
+     */
+    @NotNull
+    public static Vector projection(final @NotNull Vector onto, final @NotNull Vector u) {
+        return onto.clone().multiply(onto.dot(u) / onto.lengthSquared());
+    }
+
 }
--- api/src/main/kotlin/dev/hawu/plugins/api/dsl/misc/Maths.kt
+++ api/src/main/kotlin/dev/hawu/plugins/api/dsl/misc/Maths.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNUSED")
+
 package dev.hawu.plugins.api.dsl.misc
 
 import dev.hawu.plugins.api.MathUtils
@@ -8,3 +10,46 @@ import org.bukkit.util.Vector
  * NaNs if the vector's length is 0.
  */
 fun Vector.safeNormalize() = MathUtils.normalize(this)
+
+/**
+ * Rotates a vector around the X axis with the angle
+ * in degrees.
+ */
+fun Vector.rotateX(degree: Double) = MathUtils.rotateAroundAxisX(this, degree)
+
+/**
+ * Rotates a vector around the Y axis with the angle
+ * in degrees.
+ */
+fun Vector.rotateY(degree: Double) = MathUtils.rotateAroundAxisY(this, degree)
+
+/**
+ * Rotates a vector around the Z axis with the angle
+ * in degrees.
+ */
+fun Vector.rotateZ(degree: Double) = MathUtils.rotateAroundAxisZ(this, degree)
+
+/**
+ * Rotates a vector around the X axis with precomputed trig.
+ */
+fun Vector.rotateX(cos: Double, sin: Double) = MathUtils.rotateAroundAxisX(this, cos, sin)
+
+/**
+ * Rotates a vector around the Y axis with precomputed trig.
+ */
+fun Vector.rotateY(cos: Double, sin: Double) = MathUtils.rotateAroundAxisY(this, cos, sin)
+
+/**
+ * Rotates a vector around the Z axis with precomputed trig.
+ */
+fun Vector.rotateZ(cos: Double, sin: Double) = MathUtils.rotateAroundAxisZ(this, cos, sin)
+
+/**
+ * Finds the perpendicular part of [this] onto [other].
+ */
+fun Vector.perpendicular(other: Vector) = MathUtils.perpendicular(this, other)
+
+/**
+ * Finds the projection of [this] onto [other].
+ */
+fun Vector.project(other: Vector) = MathUtils.projection(this, other)
